e645d99428ea71dd937695fe932bc01304ca4510
David Choqueluque Roman <davidchoqueluqueroman@iMac-de-David.local>
2019-08-23 06:05:53 -0500

    resnet

--- ResNet.py
+++ ResNet.py
@@ -7,25 +7,58 @@ class ViolenceModelResNet(nn.Module):
     def __init__(self, seqLen, feature_extract=True):
         super(ViolenceModelResNet, self).__init__()
         self.seqLen = seqLen
+
         self.model_ft = models.resnet18(pretrained=True)
 
-        self.set_parameter_requires_grad(self.model_ft, feature_extract)
+        self.conv1 = nn.Sequential(*list(self.model_ft.conv1.children()))
+        self.bn1 = nn.Sequential(*list(self.model_ft.bn1.children()))
+        self.relu = nn.Sequential(*list(self.model_ft.relu.children()))
+        self.maxpool = nn.Sequential(*list(self.model_ft.maxpool.children()))
+        
+        self.l1 = nn.Sequential(*list(self.model_ft.layer1.children()))
+        self.l2 = nn.Sequential(*list(self.model_ft.layer2.children()))
+        self.l3 = nn.Sequential(*list(self.model_ft.layer3.children()))
+        self.l4 = nn.Sequential(*list(self.model_ft.layer4.children()))
+
+        self.avgpool = nn.Sequential(*list(self.model_ft.avgpool.children()))
+
+        self.set_parameter_requires_grad(self.conv1, feature_extract)
+        self.set_parameter_requires_grad(self.bn1, feature_extract)
+        self.set_parameter_requires_grad(self.relu, feature_extract)
+        self.set_parameter_requires_grad(self.maxpool, feature_extract)
+        self.set_parameter_requires_grad(self.l1, feature_extract)
+        self.set_parameter_requires_grad(self.l2, feature_extract)
+        self.set_parameter_requires_grad(self.l3, feature_extract)
+        self.set_parameter_requires_grad(self.l4, feature_extract)
+        self.set_parameter_requires_grad(self.avgpool, feature_extract)
         
         self.num_ftrs = self.model_ft.fc.in_features
-        self.model_ft.fc = nn.Linear(self.seqLen*self.num_ftrs, 2)
+        self.fc = nn.Linear(self.seqLen*self.num_ftrs, 2)
     
     def forward(self, x):
         lista = []
+
         for dimage in range(0, self.seqLen):
-            feature = self.convNet(x[dimage])
-        #       print('--->feature  (CNN output) size: ',feature.size())
-            feature = feature.view(feature.size(0), 256 * 6 * 6)
-            lista.append(feature)
+            x = self.convNet(x[dimage])
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+            x = self.maxpool(x)
+            x = self.l1(x)
+            x = self.l2(x)
+            x = self.l3(x)
+            x = self.l4(x)
+
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+            x = x.view(x.size(0), self.num_ftrs)
+            lista.append(x)
     #       print('--->feature VIEW (CNN output) size: ',feature.size())
         
         x = torch.cat(lista, dim=1)  
     #     print('x cat: ',x.size())
-        x = self.linear(x)
+        x = self.fc(x)
         
     #     print('x classifier: ',x.size())
         
